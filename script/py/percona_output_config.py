75b52ed0e0f5deb002335c8a14f9c920fa9510bb
lwxwx <814237659@qq.com>
2020-06-29 21:19:29 +0800

    Update percona_output_config.py

--- script/py/percona_output_config.py
+++ script/py/percona_output_config.py
@@ -19,24 +19,24 @@ config_path = "./config.ini" # sys.argv[1]
 cfg = ConfigParser()
 cfg.read(config_path)
 
-build_path = cfg.get("build_env","build_dir")
-out_dir = cfg.get("build_env","out_dir")
-port = int(cfg.get("build_env","port"))
-xcom_port = int(cfg.get("build_env","xcom_port"))
+build_path = cfg.get("build_env","build_dir")     #mysql的安装目录，环境目录啥的，好像也可以直接用
+out_dir = cfg.get("build_env","out_dir")          #data目录放在这个下面
+port = int(cfg.get("build_env","port"))           #端口号
+xcom_port = int(cfg.get("build_env","xcom_port")) #xcom的端口号，现在用的不是xcom了，和上面的端口号不一样就行
 #user = cfg.get("build","user")
 mysqld_path=build_path+"/bin/mysqld"
 
-node_count = int(cfg.get("run_env","node_count")) #sys.argv[2]
-group_name = cfg.get("run_env","group_name")#"mmlp_group"
-local_ip = cfg.get("run_env","local_ip")#"10.11.6.120"
-remote_peer_nodes = cfg.get("run_env","remote_peer_nodes")
+node_count = int(cfg.get("run_env","node_count"))  #sys.argv[2] #有几个节点 1
+group_name = cfg.get("run_env","group_name")       #"mmlp_group" #现在没啥用
+local_ip = cfg.get("run_env","local_ip")           #"10.11.6.120" #本地ip
+remote_peer_nodes = cfg.get("run_env","remote_peer_nodes")# 远程IP：port,...
 
 if(remote_peer_nodes == ""):
     peer_nodes_str = ""
 else:
     peer_nodes_str = remote_peer_nodes + ","
 
-for j in range(0,node_count):
+for j in range(0,node_count):                       #有几个节点端口号就依次递增
     peer_nodes_str = peer_nodes_str + local_ip + ":" + str(xcom_port+j)
     # if(j < node_count - 1 and node_count > 1):
     #     peer_nodes_str += ","
@@ -62,7 +62,7 @@ for i in range(0,node_count):
             "password": "123"
         }
         peer_nodes_str_i = peer_nodes_str.replace(local_ip+":"+str(xcom_port+i)+",","")
-        peer_nodes_str_i = peer_nodes_str_i.rstrip(",")
+        peer_nodes_str_i = peer_nodes_str_i.rstrip(",")     #删除字符串末尾的逗号
         #print(peer_nodes_str_i)
         mysql_config_i["mysqld"] = {
             "port":str(port+i),
@@ -76,9 +76,9 @@ for i in range(0,node_count):
             "multi_master_log_plugin_peer_nodes": peer_nodes_str_i,
             "symbolic-links" : "0"
         }
-        with open(config_i,'w') as configfile:
+        with open(config_i,'w') as configfile: #写配置文件
             mysql_config_i.write(configfile)
-        a,b = subprocess.getstatusoutput(mysqld_path + " --defaults-file="+config_i + "  --initialize")
+        a,b = subprocess.getstatusoutput(mysqld_path + " --defaults-file="+config_i + "  --initialize")#运行并返回结果
         print("result["+str(i)+"] = " + str(a))
-        if(a != 0):
+        if(a != 0):     #0是正常的，输出错误信息
             print(b)
